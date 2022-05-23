97a70805537c34a39aa9bcf5527fcf9f51963310
username <103047154+SunaD630@users.noreply.github.com>
2022-05-23 23:17:20 +0900

    Initial Commit

---
+++ Osero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Osero
+//
+//  Created by 達巳 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
