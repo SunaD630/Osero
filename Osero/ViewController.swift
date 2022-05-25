c6927c3812cdaa2e6e5c7021bfaff69aea4029a9
username <103047154+SunaD630@users.noreply.github.com>
2022-05-25 21:37:18 +0900

    525

--- Osero/ViewController.swift
+++ Osero/ViewController.swift
@@ -6,15 +6,47 @@
 import UIKit
 
 class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    let boardsize:Int = 8
     let margin:Float = 1.0
+    var board:[[Int]] = [[0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,-1,1,0,0,0],
+                          [0,0,0,1,-1,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0]]
+    var stone = 1
+    
+    func reverse(x:Int, y:Int, board: [[Int]]){
+        for i in -1..<2{
+            for j in -1..<2{
+                if i == 0 && j == 0{break}
+                var dist:Int = 1
+                while (0<=x+i*dist && x+i*dist<boardsize && 0<=y+j*dist && y+j*dist<boardsize){
+                    if board[y+j*dist][x+i*dist] == stone{break}
+                    dist += 1
+                }
+                if (dist==1 || 0>x+i*dist || x+i*dist>=boardsize || 0>y+j*dist || y+j*dist>=boardsize){
+                    for k in 0..<dist{
+                        board[y+j*k][x+i*k] = stone
+                    }
+                    stone *= -1
+                }
+            }
+        }
+    }
+    
+    
+    //セクションの数
     internal func numberOfSections(in collectionView: UICollectionView) -> Int {
-            return 8
+            return boardsize
     }
-    //セクションの中のセルの数を返す
+    //セクションの中のセルの数
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        return boardsize
     }
-    //セルのサイズを指定する処理
+    //セルのサイズ
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
         // 横方向のスペース調整
@@ -25,7 +57,7 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         // 正方形で返すためにwidth,heightを同じにする
         return CGSize(width: cellSize, height: cellSize)
     }
-    
+    //セクション間のmargin
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: CGFloat(margin), left: CGFloat(margin), bottom: CGFloat(margin), right: CGFloat(margin))
     }
@@ -37,19 +69,33 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return CGFloat(margin)
     }
-    //セルに表示する内容を記載する
+    
+    //セルの内容
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-        //storyboard上のセルを生成
         let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
 
         //セル上のTag(1)とつけたUILabelを生成
         let label = cell.contentView.viewWithTag(1) as! UILabel
 
-        //今回は簡易的にセルの番号をラベルのテキストに反映させる
-        label.text = String(indexPath.section*8 + indexPath.row + 1)
+        switch board[indexPath.section][indexPath.row]{
+        case 1:
+            label.text = "o"
+        case -1:
+            label.text = "・"
+        default:
+            label.text = ""
+        }
 
         return cell
+    }
+    // Cell が選択された場合
+    func collectionView(_ collectionView: UICollectionView,
+                              didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)!
+        let label = cell.contentView.viewWithTag(1) as! UILabel
+        
+        label.text = "o"
     }
 
     override func viewDidLoad() {
